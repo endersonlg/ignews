387720ce852ed306ac884702f462614360734a02
endersonlg <endersonluciog@gmail.com>
2021-08-12 00:09:08 -0300

    integracao com api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,22 @@
+import {GetStaticProps} from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from './home.module.scss'
 import {SubcribeButton} from '../components/SubscribeButton'
 
 import avatar from '../../public/images/avatar.svg'
+import { stripe } from '../services/stripe'
+
+interface HomeProps {
+  product:{
+    priceId:string,
+    amount:string
+  }
+}
 
 
-export default function Home() {
+export default function Home({product}:HomeProps) {
+  console.log(typeof product.amount)
   return (
     <>
       <Head>
@@ -18,12 +28,32 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br/>
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-        <SubcribeButton/>
+        <SubcribeButton priceId={product.priceId}/>
         </section>
         <Image src={avatar} alt="Girl coding"/>;
       </main>
     </>
   );
+}
+
+
+export const getStaticProps:GetStaticProps = async()=>{
+  const price = await stripe.prices.retrieve('price_1JNTeCBQuz6Ak0ozqtHHjLmk')
+
+  const product = {
+    priceId:price.id,
+    amount: new Intl.NumberFormat('en-US',{
+      style:'currency',
+      currency: 'USD'
+    }).format(price.unit_amount /100)
+  }
+
+  return {
+    props:{
+      product
+    },
+    revalidate:60*60*24 //24 hours
+  }
 }
